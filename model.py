0e1d010ccfd2314ec76cf1d63d3c7b6d6b9710f1
Nicolas Kolbenschlag <nicolas.kolbenschlag@googlemail.com>
2020-11-07 12:33:39 +0100

    upload

--- model.py
+++ model.py
@@ -1,6 +1,8 @@
 import transformers
 import torch
 import torch.nn
+import os
+import pandas as pd
 
 MODEL = "bert-base-cased"
 SEQ_LEN = 512
@@ -47,4 +49,24 @@ class RegressionModel(torch.nn.Module):
 
     def forward(self, input_ids, attention_mask):
         pass
+
+if __name__ == "__main__":
+    # if torch.cuda.is_available():       
+    #     device = torch.device("cuda")
+    #     print(f"There are {torch.cuda.device_count()} GPU(s) available.")
+    #     print("Device name:", torch.cuda.get_device_name(0))
+    # else:
+    #     print("No GPU available, using the CPU instead.")
+    #     device = torch.device("cpu")
         
+    directory_data = "../../EmCaR/"
+    directory_data += "2_data/360p/transcriptions_new/"
+
+    directory_label = "wild/label_segments/valence/"
+    
+    # texts = []
+    for filename in os.listdir(directory_data):
+        df_data = pd.read_csv(directory_data + filename, sep="\t")
+
+        df_label = pd.read_csv(directory_label + filename.split("_")[0] + ".csv", sep=",")
+        break
