cad0a9023f8600ed42f959038a5c65ce4afc5ca7
Nicolas Kolbenschlag <nicolas.kolbenschlag@googlemail.com>
2021-01-30 14:07:56 +0100

    upload

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@
 export START_HERE=/nas/student/NicolasKolbenschlag/emotion_uncertainty
 
 source $START_HERE/venv/bin/activate
-python3 $START_HERE/MuSe-LSTM-Attention-baseline-model/extract_features/emotion_recognition/main.py --feature_set bert-4 --emo_dim_set valence --epochs 100 --refresh --n_seeds 1 --seed 314 --attn --rnn_bi --loss ccc --rnn_dr .3 --attn_dr .3 --out_dr .3 --predict
+python3 $START_HERE/MuSe-LSTM-Attention-baseline-model/extract_features/emotion_recognition/main.py --feature_set bert-4 --emo_dim_set valence --epochs 100 --refresh --n_seeds 1 --seed 314 --attn --rnn_bi --loss ccc --rnn_dr .3 --attn_dr .3 --out_dr .3 --uncertainty_approach monte_carlo_dropout --predict
